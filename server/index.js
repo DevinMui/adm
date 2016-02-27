2cd7f32f006011d1693929b6a968e8d097abcb2d
Devin <devinmui@yahoo.com>
2016-02-26 22:17:27 -0800

    uncommit stuff

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var twilio = require('twilio')
+var express = require('express')
+var app = express()
+
+var client = new twilio.RestClient(accountSid, authToken)
+
+app.get('/red', function(){
+	// twilio send
+	client.messages.create({
+    to:'+16512223344', // number of sim
+    from:'TWILIO_NUMBER',
+    body:'red'
+	}, function(error, message) {
+	    if (error) {
+	        console.log(error.message);
+	    }
+	});
+})
+
+app.get('/green', function(){
+	// twilio send
+	client.messages.create({
+    to:'+16512223344', // number of sim
+    from:'TWILIO_NUMBER',
+    body:'green'
+	}, function(error, message) {
+	    if (error) {
+	        console.log(error.message);
+	    }
+	});
+})
+
+app.listen(3000, function () {
+	console.log('App running on port 3000');
+});
