b974b78c03321f79387f5d76dca873067dd41e9f
Devin <devinmui@yahoo.com>
2016-02-27 01:28:06 -0800

    index

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 var twilio = require('twilio')
+var request = require('request')
 var tokens = require('./tokens')
 var express = require('express')
 var app = express()
 
 var accountSid = tokens.accountSid
 var authToken = tokens.authToken
+var arcToken = tokens.arcToken
 
 var client = new twilio.RestClient(accountSid, authToken)
 
 var twilioNumber = "+15005550006"
 
 var number = "+16512223344"
+
+var location = ""
 
 app.get('/red', function(req, res){
 	// twilio send
@@ -42,6 +46,68 @@ app.get('/green', function(req, res){
     	res.send("GREEN LIGHT")
     }
 	});
+})
+
+var directionsOptions = {
+	url: 'http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve/',
+	json: true,
+	form: directionsParams
+}
+
+app.post('/location', function(req, res){
+	location = req.body.location
+
+	var initX, initY, destinationX, destinationY = 0
+
+	// find place's x and y
+	var findInitParams = {
+		f: 'json',
+		token: arcToken,
+		text: location
+	}
+
+	var findInitOptions = {
+		url: 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find/',
+		json: true,
+		form: findInitParams
+	}
+
+	request.post(findInitOptions, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	console.log(body)
+    	initX = body.x
+    	initY = body.y
+  	}
+	})
+
+	// find place's x and y
+	var findDesitinationParams = {
+		f: 'json',
+		token: arcToken,
+		text: location
+	}
+
+	var findDestinationOptions = {
+		url: 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find/',
+		json: true,
+		form: findInitParams
+	}
+
+	request.post(findDestinationOptions, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	console.log(body)
+    	destinationX = body.x
+    	destinationY = body.y
+  	}
+	})
+
+	// directions
+	var directionsParams = {
+		f: 'json',
+		token: arcToken,
+		stops: initX + ',' + initY + ';' + destinationX + ',' + destinationY
+	}
+
 })
 
 app.listen(3000, function () {
