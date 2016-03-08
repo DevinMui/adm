5b3712090c7f1124159292a6eae64406e092c484
pepe-roni <ahde2k@gmail.com>
2016-03-07 23:48:50 -0800

    working ESC and refined accerlation method

--- servo_motor/ESC_and_Electronic_Steering/ESC_and_Electronic_Steering.ino
+++ servo_motor/ESC_and_Electronic_Steering/ESC_and_Electronic_Steering.ino
@@ -1,21 +1,47 @@
 #include <Servo.h>
 Servo steering, ESC;
-int turn = 90; 
-int throttle=105; 
+int turn = 90;
+int throttle = 95;
+boolean shift;
 
 void setup()
 {
+  //Serial.begin(9200);
   ESC.attach(0);
+  delay(200);
   ESC.write(105);
+  delay(200);
+  ESC.write(90);
+  delay(200);
 }
 
 void loop()
 {
-  //zero = 105 
- ESC.write(throttle); 
- delay(200);
- throttle+0.2;
+  //Serial.println(throttle);
+  //Serial.print(shift);
+  //stop = 95
+  ESC.write(throttle);
+  delay(50);
+  esc();
+  if (shift)
+  {
+    throttle -= 0.5;
+  }
+  else
+  {
+    throttle++;
+  }
 }
 
+void esc()
+{
+  if (throttle <= 1)
+  {
+    shift = false;
+  }
+  else if (throttle >= 95)
+    shift = true;
+
+}
 
 
