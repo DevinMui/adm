a383a571b0639ad5d03b47c534dc1bd7ef3fb207
pepe-roni <ahde2k@gmail.com>
2016-03-22 23:59:01 -0700

    ez lcd

--- servo_motor/ESC_and_Electronic_Steering/ESC_and_Electronic_Steering.ino
+++ servo_motor/ESC_and_Electronic_Steering/ESC_and_Electronic_Steering.ino
@@ -1,27 +1,42 @@
 #include <Servo.h>
+#include <LiquidCrystal.h>
+#include <LCDKeypad.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 Servo steering, ESC;
+
 int turn = 90;
 int throttle = 95;
 boolean shift;
 
 void setup()
 {
-  //Serial.begin(9200);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("ADM Initializing");
+  delay(1000);
+  
   ESC.attach(0);
   delay(200);
   ESC.write(105);
   delay(200);
   ESC.write(90);
   delay(200);
+  
+  lcd.setCursor(0, 0);
+  lcd.print("   ADM Ready!   ");
+  delay(500);
 }
 
 void loop()
 {
-  //Serial.println(throttle);
-  //Serial.print(shift);
+  lcd.setCursor(0,1);
+  lcd.print("SPEED: ");
+  lcd.setCursor(7,1);
+  lcd.print(throttle);
   //stop = 95
   ESC.write(throttle);
-  delay(50);
+  delay(250);
   esc();
   if (shift)
   {
@@ -35,13 +50,12 @@ void loop()
 
 void esc()
 {
-  if (throttle <= 1)
+  if (throttle <= 70)
   {
     shift = false;
   }
   else if (throttle >= 95)
     shift = true;
-
 }
 
 
